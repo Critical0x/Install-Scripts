27685e232d13cb837685f78573dc006e52ab404d
pods <103599644+Critical0x@users.noreply.github.com>
2023-01-14 22:09:26 -0500

    Create arch-applications.sh

---
+++ arch-applications.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#########################
+
+sudo pacman -S thunar lynis bitwarden fish neovim alacritty volumeicon virt-manager qemu-desktop dnsmasq iptables-nft neovim emacs flameshot htop   
